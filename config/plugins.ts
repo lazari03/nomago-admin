37607b116cbb303208ca66f714c2cf5dab750492
lazari03 <alfonzlazri@yea.al>
2025-07-18 03:01:25 +0200

    d3

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,13 +3,13 @@ export default () => ({
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: process.env.S3_ACCESS_KEY_ID,
-        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-        region: process.env.S3_REGION || 'us-east-1',
-        endpoint: process.env.S3_ENDPOINT,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION || 'us-east-1',
+        endpoint: process.env.AWS_ENDPOINT,
         s3ForcePathStyle: true,
         params: {
-          Bucket: process.env.S3_BUCKET,
+          Bucket: process.env.AWS_BUCKET,
         },
       },
     },
