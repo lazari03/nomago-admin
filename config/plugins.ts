de7bc85dd5f96039b69da4487e8de68798488f4c
lazari03 <alfonzlazri@yea.al>
2025-07-18 03:08:21 +0200

    configured s3 bucket

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,14 +3,17 @@ export default () => ({
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        region: process.env.AWS_REGION || 'us-east-1',
-        endpoint: process.env.AWS_ENDPOINT,
-        s3ForcePathStyle: true,
-        params: {
-          Bucket: process.env.AWS_BUCKET,
+        s3Options: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+          region: process.env.AWS_REGION || 'us-east-1',
+          endpoint: process.env.AWS_ENDPOINT,
+          s3ForcePathStyle: true,
+          params: {
+            Bucket: process.env.AWS_BUCKET,
+          },
         },
+        baseUrl: process.env.AWS_ENDPOINT + '/' + process.env.AWS_BUCKET, // Add this line if supported
       },
     },
   },
