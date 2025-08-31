5ffc4b2f841a6a9f4a21f6fdbd3d265a7131c2c7
lazari03 <alfonzlazri@yea.al>
2025-08-31 17:56:33 +0200

    domain change

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -9,7 +9,7 @@ export default [
       origin: [
         'http://localhost:3000', // Next.js local dev
         'http://localhost:8081', // React Native or other local dev
-        'http://xgs8swck0g8cgs8gcososwg8.168.231.78.121.sslip.io'
+        'http://og0w444gsssg0ks0gwwogk40.168.231.78.121.sslip.io/listings'
       ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       headers: '*',
