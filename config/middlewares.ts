f42fcbfc7d77b4e1a93871bed4c42dbc25f84cc6
lazari03 <alfonzlazri@yea.al>
2025-09-05 20:48:08 +0200

    cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -10,7 +10,8 @@ export default [
         'http://localhost:3000', // Next.js local dev
         'http://localhost:8081', // React Native or other local dev
         'http://xgs8swck0g8cgs8gcososwg8.168.231.78.121.sslip.io',
-        'http://og0w444gsssg0ks0gwwogk40.168.231.78.121.sslip.io'
+        'http://og0w444gsssg0ks0gwwogk40.168.231.78.121.sslip.io',
+        'https://management.nomago.al'
 
         
       ],
